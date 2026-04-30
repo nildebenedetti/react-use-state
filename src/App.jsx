7c560947579eb7fd90defe1be7a3e1bd29bd2539
“Nilde <nilde.benedetti@gmail.com>
2026-04-30 15:57:41 +0200

    add: import languages.js in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Card from "./components/Card";
+import languages from "./components/data/languages";
+
 
 function App() {
   return (
     <div className="container py-3 mx-auto d-flex justify-content-center">
-      <Card 
-      title = "Titolo"
-      content = "MiaoMiaoMiaoMiaoMiaoMiaoMiaoMiaoMiaoMiaoMiaoMiaoMiao"
-      />
+
+      <Card/>
+
     </div>
   );
 }
