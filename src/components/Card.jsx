050dbd61b677ff63ad7a552ea8dcc562000f68cb
“Nilde <nilde.benedetti@gmail.com>
2026-04-30 16:20:18 +0200

    add: variabile let per gestire il cambiamento di  colore del bottone

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,13 @@ function Card({
     title = "My Language",
     content = "A wonderful description of language's peculiarities and fun facts"
 }) { 
+    {/**variabile per gestire cambiamento del colore della variabile */}
+    let btnColor = 'btn-primary';
 
     return <>
         <div className="card">
                 <div className="card-body">
-                    <a href="#" className="btn btn-primary">{title}</a>
+                    <a href="#" className={`btn ${btnColor}`}>{title}</a>
                     <p className="card-text py-1">{content}</p>
                 </div>
         </div>
