21bd3d7a1b38180f07798272b25c0a352ea0cd0e
“Nilde <nilde.benedetti@gmail.com>
2026-04-30 16:06:59 +0200

    add: map per render Card.jsx per ogni oggetto del dataset in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,13 @@ import languages from "./components/data/languages";
 function App() {
   return (
     <div className="container py-3 mx-auto d-flex justify-content-center">
-
-      <Card/>
+      
+      {languages.map( language => {
+        const { id, title, description} = language;
+        return <Card key={id}
+                title = {title}
+                content = {description}/>
+      })}
 
     </div>
   );
