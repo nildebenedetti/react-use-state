c8fa75edee0977d78deb868aca928a8f8f81b585
“Nilde <nilde.benedetti@gmail.com>
2026-04-30 16:16:45 +0200

    add: row div + grid structure style in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import languages from "./data/languages";
 function App() {
   return (
     <div className="container py-3 mx-auto d-flex justify-content-center">
-      
-      {languages.map( language => {
-        const { id, title, description} = language;
-        return <Card key={id}
-                title = {title}
-                content = {description}/>
-      })}
-
+      <div className="row row-cols-1 gap-4 px-4">
+        {languages.map(language => {
+          const { id, title, description } = language;
+          return <Card key={id}
+            title={title}
+            content={description} />
+        })}
+      </div>
     </div>
   );
 }
