f9a138ae2226931081f53b461f8ec9f1e2af9eb1
“Nilde <nilde.benedetti@gmail.com>
2026-04-30 15:42:36 +0200

    add: component props + placeholder text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,15 @@
-function Card() {
+{/* aggiungo lóggetto props direttamente con il destructuring 
+    e assegno valore per creare destructuring */}
+
+function Card({ 
+    title = "My Language",
+    content = "A wonderful description of language's peculiarities and fun facts"
+}) { 
+
     return <>
         <div className="card">
                 <div className="card-body">
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <a href="#" className="btn btn-primary">{`${title}`}</a>
                     <p className="card-text py-1">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
         </div>
